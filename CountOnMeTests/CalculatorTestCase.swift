726580eae11191af35508ebc9934a8829c443af9
“Yohan <>
2020-04-20 03:23:08 +0200

    feat: create class calculator and set main features.

---
+++ CountOnMeTests/CalculatorTestCase.swift
@@ -0,0 +1,15 @@
+//
+//  CalculatorTestCase.swift
+//  CountOnMeTests
+//
+//  Created by Yohan William Dunon on 19/04/2020.
+//  Copyright © 2020 Vincent Saluzzo. All rights reserved.
+//
+
+import XCTest
+@testable import CountOnMe
+
+class CalculatorTestCase: XCTestCase {
+    func testGivenExpressionIsCorrectWhenTappedEqualButtonThenResultShouldBeDisplayed() { 
+    }
+}
